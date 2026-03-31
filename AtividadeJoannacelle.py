70606e9165db63d53e9b3060572561456cecf173
Bruno-Sa-fr <bfranca884@gmail.com>
2026-03-31 04:50:19 -0300

    Add files via upload

---
+++ AtividadeJoannacelle.py
@@ -0,0 +1,42 @@
+b = float(input("Digite o primeiro número: "))
+a = float(input("Digite o segundo número: "))
+resultado = a * b
+print("O resultado da multiplicação entre a e b é:", resultado)
+
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+c = float(input("Digite o terceiro número: "))
+d = float(input("Digite o quarto número: "))
+e = float(input("Digite o quinto número: "))
+media_aritmetica = (a + b + c + d + e) / 5
+print("A média aritmética é dos 5 números é 8:", media_aritmetica)
+
+
+valor = float(input("Digite o valor do produto: "))
+imposto = valor * 0.08
+preco_final = valor + imposto
+print("O preço final com 8% de imposto é:", preco_final)
+
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+resultado = a - b
+print("O resultado da subtração é:", resultado)
+
+
+peso = float(input("Digite o peso (kg): "))
+altura = float(input("Digite a altura (m): "))
+imc = peso / (pow(altura,altura))
+print("O IMC é:", imc)
+
+
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("A temperatura em Fahrenheit é:", fahrenheit)
+
+
+horas = float(input("Digite a quantidade de horas trabalhadas: "))
+valor = float(input("Digite o valor da hora trabalhada: "))
+salario = horas * valor
+print("O salário total é:", salario)
